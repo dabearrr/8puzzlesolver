a0d01410c72f116895482b6f4801943860e0baef
dabearrr <dabearrr@gmail.com>
2016-10-20 14:07:48 -0700

    Added helper class to state locations in 2d array, added test goal board, added structure for calling moves (operators) in board, began strucuture of of Uniform cost search."

--- 8puzzle.py
+++ 8puzzle.py
@@ -1,51 +1,116 @@
 #for creating the tree itself
 class TreeNode:
-	def __init__(self, data):
-		self.data = data
-		self.children = []
-	def append(self, newNode):
-		self.children.append(newNode)
+    def __init__(self, data):
+        self.data = data
+        self.children = []
+    def append(self, newNode):
+        self.children.append(newNode)
+
+class Pair:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
 #used to store the board state itself, also has a compare func, display func
 class Board:
+    def __init__(self):
+        self.state = []
     def __init__(self, initial_state):
-        state = initial_state
+        self.state = initial_state
     def isEqual(self, boardB):
-        for i in range(0, len(boardB)):
-            for j in range(0, len(boardB)):
+        for i in range(0, len(boardB.state)):
+            for j in range(0, len(boardB.state)):
                 if(self.state[i][j] != boardB.state[i][j]):
                     return False
         return True
+    def getZeroLocation(self):
+        for i in range(0, len(self.state)):
+            for j in range(0, len(self.state)):
+                if self.state[i][j] == 0:
+                    return Pair(j, i)
     def display(self):
-        for i in range(0, len(board)):
-            for j in range(0, len(board)):
-                print repr(self.state[i][j]) + " "
+        for i in range(0, len(self.state)):
+            for j in range(0, len(self.state)):
+                print repr(self.state[i][j]),
+            print " "
+    def moveUp(self):
+        loc = self.getZeroLocation()
+        tempVal = self.state[loc.y] [loc.x]
+
 
 #holds the Board object internally, used as a helper to the Board, performs ops on it
 class Puzzle:
-	def __init__(self, board):
-		self.board = board
-	def display(self):
-		board.display()
-# 	def isGoal(self):
-# 		if(board[len(board) - 1][len(board - 1)] != 0):
-# 			return False
-# 		for i in range(0, len(board):
-# 			for j in range
+    def __init__(self, board):
+        self.board = board
+    def display(self):
+        self.board.display()
+    def isGoal(self, goalBoard):
+        return self.board.isEqual(goalBoard)
+    def getLegalMoves(self):
+        tempPair = self.board.getZeroLocation()
+        legalMoves = []
+        if(tempPair.x > 0):
+            legalMoves.append("l")
+        if(tempPair.y > 0):
+            legalMoves.append("u")
+        if(tempPair.x < len(self.board.state) - 1):
+            legalMoves.append("r")
+        if(tempPair.y < len(self.board.state) - 1):
+            legalMoves.append("d")
+        return legalMoves
+    def move(self, direction):
+        if(direction == "u"):
+            self.board.moveUp()
+        elif(direction == "r"):
+            self.board.moveRight()
+        elif (direction == "d"):
+            self.board.moveDown()
+        elif (direction == "l"):
+            self.board.moveLeft()
+
+def findSolution(puzzle, goal):
+    # create the root
+    root = TreeNode(puzzle)
+    queue = []
+    queue.append(root)
+
+    while queue:
+        legalMoves = queue[0].data.getLegalMoves()
+        for item in legalMoves
+            tempPuzzle = queue[0]
+            tempPuzzle.move(item)
+            queue[0].append(TreeNode(tempPuzzle))
 
 #take in input
 userInput = raw_input("Welcome to Raymond Farias's puzzle solver: Enter 1 for the default puzzle, or 2 to enter your own.")
-if(userInput == 1):
+startBoard = []
+endBoard = []
+if(int(userInput) == 1):
     #make default board
-    defBoard = []
-    defBoard.append([1, 2, 0])
-    defBoard.append([4, 5, 3])
-    defBoard.append([7, 8, 6])
-elif(userInput == 2):
+    startBoard = []
+    startBoard.append([1, 2, 0])
+    startBoard.append([4, 5, 3])
+    startBoard.append([7, 8, 6])
+
+    endBoard.append([1, 2, 3])
+    endBoard.append([4, 5, 6])
+    endBoard.append([7, 8, 0])
+elif(int(userInput) == 2):
     #take in user board
     print("Enter your puzzle, enter a zero to represent the blank.")
 else:
     print ("Error, please enter a correct number.")
-    
-userBoard = Board(defBoard)
+    raise "InputError"
+
+
+#board test
+userBoard = Board(startBoard)
 userBoard.display()
+
+goalBoard = Board(endBoard)
+
+#puzzle test
+userPuzzle = Puzzle(userBoard)
+userPuzzle.display()
+print userPuzzle.getLegalMoves()
+print userPuzzle.isGoal(goalBoard)
